185fe898f61cdb902e32924004d78a50e5320dfb
Juan F. Ruiz <singularidad.cuantica@gmail.com>
2025-05-22 19:44:18 +0200

    Traducidas las plantillas que se usan en la generación de la documentación HTML, PDF y EPUB. - Traducidos los documentos:   * script de construcción de gradle   * CONTRIBUTING   * fromhtmltohtmlviahtml.md   * scripts unix/windows de gradle   * La licencia del proyecto   * thymeleaf-tutorials.js   * thymeleaf-articles.js
    Y traducciones de la cabecera de licencia en muchos archivos fuente.

--- gradlew.bat
+++ gradlew.bat
@@ -1,27 +1,27 @@
 @rem
-@rem Copyright 2015 the original author or authors.
+@rem Copyright 2015 del autor o autores originales.
 @rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
+@rem Licenciado bajo la Licencia Apache, Versión 2.0 (la "Licencia");
+@rem No puede usar este archivo excepto de conformidad con la Licencia.
+@rem Puede obtener una copia de la Licencia en
 @rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem https://www.apache.org/licenses/LICENSE-2.0
 @rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
+@rem A menos que la ley aplicable lo exija o se acuerde por escrito, el software
+@rem distribuido bajo la Licencia se distribuye "TAL CUAL",
+@rem SIN GARANTÍAS NI CONDICIONES DE NINGÚN TIPO, ni expresas ni implícitas.
+@rem Consulte la Licencia para conocer el idioma específico que rige los permisos y las
+@rem limitaciones de la Licencia.
 @rem
 
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
+@rem  Script de inicio de Gradle para Windows
 @rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with windows NT shell
+@rem Establecer el ámbito local para las variables con el shell de Windows NT
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
@@ -29,10 +29,10 @@ if "%DIRNAME%"=="" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+@rem Resuelva cualquier "." y ".." en APP_HOME para hacerlo más corto.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+@rem Agregue aquí las opciones predeterminadas de la JVM. También puede usar JAVA_OPTS y GRADLE_OPTS para pasar opciones de la JVM a este script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
@@ -43,10 +43,10 @@ set JAVA_EXE=java.exe
 if %ERRORLEVEL% equ 0 goto execute
 
 echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo ERROR: JAVA_HOME no está configurado y no se pudo encontrar ningún comando 'java' en su PATH.
 echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+echo Configure la variable JAVA_HOME en su entorno para que coincida con la
+echo ubicación de su instalación de Java.
 
 goto fail
 
@@ -57,29 +57,29 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 if exist "%JAVA_EXE%" goto execute
 
 echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo ERROR: JAVA_HOME está configurado en un directorio no válido: %JAVA_HOME%
 echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+echo Configure la variable JAVA_HOME en su entorno para que coincida con la
+echo ubicación de su instalación de Java.
 
 goto fail
 
 :execute
-@rem Setup the command line
+@rem Configurar la línea de comandos
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 
-@rem Execute Gradle
+@rem Ejecuta Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
-@rem End local scope for the variables with windows NT shell
+@rem Finalizar el ámbito local de las variables con el shell de Windows NT
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code!
+rem ¡Establezca la variable GRADLE_EXIT_CONSOLE si necesita el código de retorno _script_ en lugar de
+rem el código de retorno _cmd.exe /c_!
 set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
